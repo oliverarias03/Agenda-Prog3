5cdc6f01b2e56b7e5619a9eefd526c84d4901359
oliverarias03 <oliverarias12@gmail.com>
2019-03-14 12:35:00 -0400

    first commit

---
+++ Tarea5/Tarea5/Controllers/CONTACTOesController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Tarea5;
+
+namespace Tarea5.Controllers
+{
+    public class CONTACTOesController : Controller
+    {
+        private TAREA_5Entities db = new TAREA_5Entities();
+
+        // GET: CONTACTOes
+        public ActionResult Index(string sortOrder, string searchString)
+        {
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "Nombre_desc" : "";
+            var contactos = from s in db.CONTACTOes select s;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                contactos = contactos.Where(s => s.NOMBRE.Contains(searchString));
+            }
+
+
+
+            switch (sortOrder)
+            {
+                case "Nombre_desc":
+                    contactos = contactos.OrderByDescending(s => s.NOMBRE);
+                    break;
+                default:
+                    contactos = contactos.OrderBy(s => s.NOMBRE);
+                    break;
+
+
+            }
+            return View(contactos.ToList());
+        }
+
+        // GET: CONTACTOes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CONTACTO cONTACTO = db.CONTACTOes.Find(id);
+            if (cONTACTO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cONTACTO);
+        }
+
+        // GET: CONTACTOes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CONTACTOes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CODIGO,NOMBRE,TELEFONO,DIRECCION")] CONTACTO cONTACTO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CONTACTOes.Add(cONTACTO);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cONTACTO);
+        }
+
+        // GET: CONTACTOes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CONTACTO cONTACTO = db.CONTACTOes.Find(id);
+            if (cONTACTO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cONTACTO);
+        }
+
+        // POST: CONTACTOes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CODIGO,NOMBRE,TELEFONO,DIRECCION")] CONTACTO cONTACTO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cONTACTO).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cONTACTO);
+        }
+
+        // GET: CONTACTOes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CONTACTO cONTACTO = db.CONTACTOes.Find(id);
+            if (cONTACTO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cONTACTO);
+        }
+
+        // POST: CONTACTOes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CONTACTO cONTACTO = db.CONTACTOes.Find(id);
+            db.CONTACTOes.Remove(cONTACTO);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Tarea5/Tarea5/Controllers/EVENTOesController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Tarea5;
+using System.Data.Entity.Core.Objects.DataClasses;
+
+namespace Tarea5.Controllers
+{
+    public class EVENTOesController : Controller
+    {
+        private TAREA_5Entities db = new TAREA_5Entities();
+
+        // GET: EVENTOes
+        public ActionResult Index(string sortOrder, string searchString)
+        {
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "Evento_desc" : "";
+            ViewBag.LugarSortParm = String.IsNullOrEmpty(sortOrder) ? "Lugar_desc" : "LUGAR";
+            ViewBag.DateSortParm = sortOrder == "FECHA" ? "Fecha_desc" : "FECHA";
+            var eventos = from s in db.EVENTOS select s;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                DateTime fecha = Convert.ToDateTime(searchString).Date;
+
+                eventos = eventos.Where(s => s.FECHA==fecha);
+
+            }
+
+            switch (sortOrder)
+            {
+                case "Evento_desc":
+                    eventos = eventos.OrderByDescending(s => s.EVENTO1);
+                    break;
+                case "Lugar_desc":
+                    eventos = eventos.OrderByDescending(s => s.LUGAR);
+                    break;
+                case "LUGAR":
+                    eventos = eventos.OrderBy(s => s.LUGAR);
+                    break;
+                case "Fecha_desc":
+                    eventos = eventos.OrderByDescending(s => s.FECHA);
+                    break;
+                case "FECHA":
+                    eventos = eventos.OrderBy(s => s.FECHA);
+                    break;
+                default:
+                    eventos = eventos.OrderBy(s => s.EVENTO1);
+                    break;
+
+
+            }
+            return View(eventos.ToList());
+        }
+
+        // GET: EVENTOes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EVENTO eVENTO = db.EVENTOS.Find(id);
+            if (eVENTO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eVENTO);
+        }
+
+        // GET: EVENTOes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EVENTOes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CODIGO,EVENTO1,LUGAR,FECHA")] EVENTO eVENTO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EVENTOS.Add(eVENTO);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(eVENTO);
+        }
+
+        // GET: EVENTOes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EVENTO eVENTO = db.EVENTOS.Find(id);
+            if (eVENTO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eVENTO);
+        }
+
+        // POST: EVENTOes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CODIGO,EVENTO1,LUGAR,FECHA")] EVENTO eVENTO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(eVENTO).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(eVENTO);
+        }
+
+        // GET: EVENTOes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EVENTO eVENTO = db.EVENTOS.Find(id);
+            if (eVENTO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eVENTO);
+        }
+
+        // POST: EVENTOes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EVENTO eVENTO = db.EVENTOS.Find(id);
+            db.EVENTOS.Remove(eVENTO);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
